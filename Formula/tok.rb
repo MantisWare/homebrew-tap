31e6a76253948572885139cac9bfc0eeb8245624
Waldo Marais <waldo@mantisware.co.za>
2026-04-20 19:24:59 +0000

    tok 0.1.2

--- Formula/tok.rb
+++ Formula/tok.rb
@@ -1,21 +1,21 @@
 class Tok < Formula
   desc "Token Optimization Kit - High-performance CLI proxy to minimize LLM token consumption"
   homepage "https://github.com/MantisWare/tok"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/MantisWare/tok/releases/download/0.1.1/tok-aarch64-apple-darwin.tar.gz"
-    sha256 "316d6c65bd1dc81754b721c8a424dad7a73a71b9cf95d4dbbbd5e720c28ea912"
+    url "https://github.com/MantisWare/tok/releases/download/0.1.2/tok-aarch64-apple-darwin.tar.gz"
+    sha256 "a6f42c2096ceb499f2bd12df4b42613b9a3d698cc08b5af0745615096b8181da"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/MantisWare/tok/releases/download/0.1.1/tok-x86_64-apple-darwin.tar.gz"
-    sha256 "43b862f9fa66a2b9e023d08d0a1158db3c5eb78d3f2c6b731dc97936ac1a7938"
+    url "https://github.com/MantisWare/tok/releases/download/0.1.2/tok-x86_64-apple-darwin.tar.gz"
+    sha256 "f3a908931dead0a8b1079a58ea03c920364e9b7b749b6015275764632b2bd713"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/MantisWare/tok/releases/download/0.1.1/tok-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "a4ccdf144895146e187bece25ae38eae6c5118cfef935a729b3e9b64f15bfe92"
+    url "https://github.com/MantisWare/tok/releases/download/0.1.2/tok-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "e1a3f155d1ab092a068f559371ccb42d87d96dddfc5e3c3d39a4c0018a243c4b"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/MantisWare/tok/releases/download/0.1.1/tok-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "b05eae2a15aeb9973ab95476a0f3af06d86471a21c5071abee22a86d9c418b60"
+    url "https://github.com/MantisWare/tok/releases/download/0.1.2/tok-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "31664091107947f6acd230b018fe117f000f7f86d7171dfce8ebf615c20f2d8b"
   end
 
   def install
