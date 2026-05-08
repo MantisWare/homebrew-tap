a4da186bb9291ae12d8d3337b9f5c115762d9aaa
Waldo Marais <waldo@mantisware.co.za>
2026-05-08 02:05:02 +0000

    tok 0.1.14

--- Formula/tok.rb
+++ Formula/tok.rb
@@ -1,21 +1,21 @@
 class Tok < Formula
   desc "Token Optimization Kit - High-performance CLI proxy to minimize LLM token consumption"
   homepage "https://github.com/MantisWare/tok"
-  version "0.1.9"
+  version "0.1.14"
   license "MIT"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/MantisWare/tok/releases/download/v0.1.9/tok-aarch64-apple-darwin.tar.gz"
-    sha256 "4724938fbf4629e05794dd8ac9b7f4304ef71bb804b49323ddb7a86fe6c2d19b"
+    url "https://github.com/MantisWare/tok/releases/download/v0.1.14/tok-aarch64-apple-darwin.tar.gz"
+    sha256 "d00022d3d81a438fcc356a2f185b240ce9b229b9c90b21abb3b594490cf5534b"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/MantisWare/tok/releases/download/v0.1.9/tok-x86_64-apple-darwin.tar.gz"
-    sha256 "ed9c4208ca63c5facef6470c2f1ce7db3dd32abf44a97f1bcea92e56a1bb2340"
+    url "https://github.com/MantisWare/tok/releases/download/v0.1.14/tok-x86_64-apple-darwin.tar.gz"
+    sha256 "a9bfbdc432ba17c4092fdebb8b405e5af0b552d7357ef9c116331eabb18e5bb2"
   elsif OS.linux? && Hardware::CPU.arm?
-    url "https://github.com/MantisWare/tok/releases/download/v0.1.9/tok-aarch64-unknown-linux-gnu.tar.gz"
-    sha256 "9bd45f0f806ffd9900e27926d33b25e1c883883b74e6488f3451785d8f1dbda6"
+    url "https://github.com/MantisWare/tok/releases/download/v0.1.14/tok-aarch64-unknown-linux-gnu.tar.gz"
+    sha256 "c9c6cab85189cb2f2b4391379893ec8fa3a58244d6e04647a910c7b391bec65f"
   elsif OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/MantisWare/tok/releases/download/v0.1.9/tok-x86_64-unknown-linux-musl.tar.gz"
-    sha256 "f88a83bab974eb1feb969740e01552632f17058c9b3b79b6cfe695d5bb29d61e"
+    url "https://github.com/MantisWare/tok/releases/download/v0.1.14/tok-x86_64-unknown-linux-musl.tar.gz"
+    sha256 "e922707d38e19403e198c54165242a411481d4997f331b1b783bb96140039973"
   end
 
   def install
@@ -31,7 +31,7 @@ class Tok < Formula
          ██║     ╚████╔╝   ██║  ██╗
          ╚═╝      ╚═══╝    ╚═╝  ╚═╝
 
-    tok 0.1.9 — Token Optimization Kit
+    tok 0.1.14 — Token Optimization Kit
     Squeeze noisy CLI output before it hits your LLM
 
   ── Quick Start ─────────────────────────────────────────
